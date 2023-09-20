0624eaf453dc8ca9594f58ac7c59652b0ab2c5ae
Erkz <Erkiparna7@gmail.com>
2023-09-20 22:37:04 +0300

    Seadistamine

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://erkiparna:vdY205oKLqr1RLPe@cluster0.uod5eyl.mongodb.net/");
+const database = mongoose.connection;
+
+database.on('error', (error: Error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(3000,() => {
+    console.log(`[server]: Server is running at http://localhost:3000`);
+});
